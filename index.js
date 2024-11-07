e1e1b36aa2c800b32700692dadde1cbff7f9712b
Giulio Calacoci <giulio.calacoci@enterprisedb.com>
2024-11-07 14:23:40 +0100

    First commit
    Signed-off-by: Giulio Calacoci <giulio.calacoci@enterprisedb.com>

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    const token = core.getInput("github_token", { required: true })
+    const number = core.getInput("numbers")
+
+    const client = github.getOctokit(token)
+    const prPull = client.pulls.get({
+        ...github.context.repo,
+        pull_number: number
+    })
+    let repoOwner = github.context.repo.owner
+    let repo = github.context.repo.repo
+
+    console.log(`Removing ${repoOwner}/${repo}/${prPull.data.head.ref}`)
+
+    client.git.deleteRef({
+        owner: repoOwner,
+        repo: repo,
+        ref: `heads/${prPull.data.head.ref}`
+    })
+
+} catch (error) {
+    core.setFailed(error.message);
+}
