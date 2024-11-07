6d65057a3473b63b4387e90e4a921a747ea70aed
Giulio Calacoci <giulio.calacoci@enterprisedb.com>
2024-11-07 14:23:40 +0100

    First commit
    Signed-off-by: Giulio Calacoci <giulio.calacoci@enterprisedb.com>

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # action-delete-pr-branch
-Simple gh action that given a pr number deletes the pr branch
+
+This action given a pr number deletes the pr head branch
+
+## Inputs
+
+### `github_token`
+
+**Required** Necessary token to execute the delete action.
+
+### `number`
+
+The number of the pull request to delete the branch from.
+
+## Example usage
+
+```yaml
+- name: Remove PRs head branch
+  uses: gcalacoci/action-delete-pr-branch@main
+  with:
+    github_token: ${{github.token}}
+    number: ${{github.event.pull_request.number}}
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function main() {
+
+    try {
+        const token = core.getInput("github_token", { required: true })
+        const number = core.getInput("number")
+
+        const client = github.getOctokit(token)
+
+        const prPull = await client.rest.pulls.get({
+            ...github.context.repo,
+            pull_number: number
+        })
+        console.log(`number ${number}`)
+        console.log(prPull)
+        let repoOwner = github.context.repo.owner
+        let repo = github.context.repo.repo
+
+        console.log(`Removing ${repoOwner}/${repo}/${prPull.data.head.ref}`)
+
+        await client.rest.git.deleteRef({
+            owner: repoOwner,
+            repo: repo,
+            ref: `heads/${prPull.data.head.ref}`
+        })
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+main()
